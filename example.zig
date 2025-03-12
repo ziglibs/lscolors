38ded197824bbed3b3ae5961fa353e7ab0a703d9
Eli <88557639+lishaduck@users.noreply.github.com>
2025-03-12 07:10:22 -0500

    docs: update example

--- example.zig
+++ example.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 const LsColors = @import("lscolors").LsColors;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
